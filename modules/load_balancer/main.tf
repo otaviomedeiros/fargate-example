9bc02c2a96e51b0b554a6094c74b3852bc6e26c8
Otavio Medeiros <otavio@bravado.co>
2022-08-15 23:22:12 -0300

    add load balancer

--- modules/load_balancer/main.tf
+++ modules/load_balancer/main.tf
@@ -29,3 +29,42 @@ resource "aws_security_group" "public_security_group" {
     Name = "App VPC - Public traffic SG"
   }
 }
+
+resource "aws_lb" "public_alb" {
+  name               = "public-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.public_security_group.id]
+  subnets            = [for subnet in var.subnets : subnet.id]
+
+  # access_logs {
+  #   bucket  = aws_s3_bucket.lb_logs.bucket
+  #   prefix  = "test-lb"
+  #   enabled = true
+  # }
+
+  tags = {
+    Environment = "production"
+  }
+}
+
+resource "aws_lb_target_group" "public_alb_target_group" {
+  name        = "target-group"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = var.vpc_id
+  target_type = "ip"
+
+  depends_on = [aws_lb.public_alb]
+}
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.public_alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.public_alb_target_group.arn
+  }
+}
